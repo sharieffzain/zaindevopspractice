aef54bc7854b179c8cf1b5bbf86617d3e4fb5455
Zain <116377569+sharieffzain@users.noreply.github.com>
2026-01-13 08:17:34 +0530

    Update p3.py

--- p3.py
+++ p3.py
@@ -1,1 +1,9 @@
-print("hello world")
+# Assign values to variables
+num1 = 5.5
+num2 = 4.5
+
+# Add two numbers
+sum_result = num1 + num2
+
+# Display the sum
+print(f"The sum of {num1} and {num2} is {sum_result}")
